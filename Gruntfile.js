078c3cd9bd308be333ece8b74281ae680337aada
zhangxinyu5663 <1976871412@qq.com>
2019-03-05 17:38:56 +0800

    grunt eslint

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'    
+      },
+      target: ['rectangle.js']    
+    }            
+  });
+
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.registerTask('default', ['eslint']);
+
+};
+
